2d37fd6e4db28eadd1161ee236f6512bacb5b464
Daniel Lima <fr.daniel.lima@gmail.com>
2017-11-24 19:21:07 -0300

    Capítulo 5

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -1,16 +1,28 @@
 class NegociacaoController {
     constructor() {
         const $ = document.querySelector.bind(document);
-
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        this._negociacoes = new Negociacoes();
     }
 
     adiciona(event) {
         event.preventDefault();
-        let negociacao = new Negociacao(
-            DateConverter.paraData(this._inputData), 
+        this._negociacoes.adiciona(this._criarNegociacao());
+        this._limparFormulario();
+     }
+
+     _limparFormulario() {
+         this._inputData.value = '';
+         this._inputQuantidade.value = 1;
+         this._inputValor.value = 0.0;
+         this._inputData.focus();
+     }
+
+     _criarNegociacao() {
+         return new Negociacao(
+            DateConverter.paraData(this._inputData.value), 
             this._inputQuantidade.value, 
             this._inputValor.value
         );
